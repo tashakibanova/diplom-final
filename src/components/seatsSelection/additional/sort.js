b029e33db5e43c0c734f961c700c626d65b71906
Natalia <99673145+tashakibanova@users.noreply.github.com>
2025-02-03 23:48:11 +0500

    Delete src directory

--- src/components/seatsSelection/additional/sort.js
+++
@@ -1,7 +0,0 @@
-export default function sort (ind, block, arr) {
-    if(arr.includes(ind)) {
-      block.push({index:ind, class: 'scheme-block-item available'})
-    } else {
-      block.push({index:ind, class: 'scheme-block-item not-available'})
-    }
-   }
--- src/store/actions/actions.js
+++
@@ -1,3 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-export const getCities = createAction("GET_CITIES");
